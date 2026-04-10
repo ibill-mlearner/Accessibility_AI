97289a66e70c78aa6ec5b233430b2380dbeb900f
my name <ibill792@uwsp.edu>
2026-04-10 04:33:07 -0500

    style update for TTS stuff

--- AccessAppFront/src/components/chat/ReadAloudControls.vue
+++ AccessAppFront/src/components/chat/ReadAloudControls.vue
@@ -31,19 +31,21 @@
       >
         Stop
       </button>
-      <label class="chat-bubble-card__volume-wrap">
-        <span class="chat-bubble-card__volume-label">Volume</span>
-        <input
-          class="form-range chat-bubble-card__volume"
-          type="range"
-          min="0"
-          max="1"
-          step="0.1"
-          :value="volume"
-          :disabled="!readAloudEnabled"
-          @input="$emit('volume', Number($event.target.value))"
-        >
-      </label>
+      <div class="chat-bubble-card__volume-panel">
+        <label class="chat-bubble-card__volume-wrap">
+          <span class="chat-bubble-card__volume-label">Volume</span>
+          <input
+            class="form-range chat-bubble-card__volume"
+            type="range"
+            min="0"
+            max="1"
+            step="0.1"
+            :value="volume"
+            :disabled="!readAloudEnabled"
+            @input="$emit('volume', Number($event.target.value))"
+          >
+        </label>
+      </div>
     </div>
   </div>
 </template>
